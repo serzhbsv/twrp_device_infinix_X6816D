7e99ddd0d0b9ab7f97c957a53aad6d101882ecfd
IMYdev <omranmansour096@gmail.com>
2023-12-08 21:44:41 +0200

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_leo.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_leo-user \
+    twrp_leo-userdebug \
+    twrp_leo-eng
---
+++ twrp_leo.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from leo device
+$(call inherit-product, device/infinix/leo/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := leo
+PRODUCT_NAME := twrp_leo
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix HOT 12 Play NFC
+PRODUCT_MANUFACTURER := infinix
