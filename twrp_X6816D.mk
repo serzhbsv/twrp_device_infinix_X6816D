3b20e76a5d28e4ebcc5399b31fc4ded7b235f994
Veynamer <113140575+Veynamer@users.noreply.github.com>
2024-01-09 22:21:24 +0300

    Update twrp_X6816D.mk

--- twrp_X6816D.mk
+++ twrp_X6816D.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from leo device
-$(call inherit-product, device/infinix/leo/device.mk)
+# Inherit from X6816D device
+$(call inherit-product, device/infinix/X6816D/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
